3042b0c69b8db0669e451accf08015a96e22589a
Luat Nguyen <luatdinhnguyen18@gmail.com>
2022-10-26 17:21:59 -0400

    Created MainActivity and CprPerformanceActivity. Added the redirect from MainActivity to CprPerformanceActivity.

---
+++ app/src/main/java/com/example/cprfeedbackapp/CprPerformanceActivity.java
@@ -0,0 +1,14 @@
+package com.example.cprfeedbackapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CprPerformanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpr_performance);
+    }
+}
--- app/src/main/java/com/example/cprfeedbackapp/MainActivity.java
+++ app/src/main/java/com/example/cprfeedbackapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.cprfeedbackapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button cprButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setup();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        cprButton.setOnClickListener(view -> goToCprPerformance());
+    }
+
+    protected void setup()
+    {
+        cprButton = findViewById(R.id.cprButton);
+    }
+
+    protected void goToCprPerformance()
+    {
+        Intent intent = new Intent(this, CprPerformanceActivity.class);
+        startActivity(intent);
     }
 }
