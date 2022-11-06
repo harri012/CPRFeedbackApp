c32fb8486a2435f61ee1c33dd67ba5a6c283809c
Justin Goi <justinlygoi@gmail.com>
2022-11-05 22:21:42 -0400

    Basic Bluetooth Class Setup

--- app/src/main/java/com/example/cprfeedbackapp/Bluetooth.java
+++ app/src/main/java/com/example/cprfeedbackapp/Bluetooth.java
@@ -1,4 +1,37 @@
 package com.example.cprfeedbackapp;
 
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+
 public class Bluetooth {
+    //Declaring data members for bluetooth connection
+    BluetoothAdapter btAdapter;
+    BluetoothDevice btDevice;
+    BluetoothSocket btSocket;
+    BluetoothServerSocket btServerSocket;
+
+    //Checks if bluetooth is enabled
+    private boolean checkBluetoothEnabled(){
+        return false;
+    }
+
+    //Search for discoverable devices
+    private void findBluetoothDevice(){}
+
+    //Checks devices that are already paired
+    private void queryPairedDevice(){}
+
+    //Creates connection
+    private void connectBluetoothDevice(){}
+
+
+    //Input Stream of Bluetooth Data
+    private String getInputData(){
+        return "";
+    }
 }
+
+
