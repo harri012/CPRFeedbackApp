4d6fd92eb73b7b5ba362db4d1c7cb65cd946dd4c
harri012 <harrisson.tang12@gmail.com>
2022-11-29 16:30:39 -0500

    -merged SOS button with new UI

--- app/src/main/java/com/example/cprfeedbackapp/HomeFragment.java
+++ app/src/main/java/com/example/cprfeedbackapp/HomeFragment.java
@@ -1,13 +1,19 @@
 package com.example.cprfeedbackapp;
 
+import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
