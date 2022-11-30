f6ce7bf034ef06ca812570f3c613d0540787df0a
Maya OH <mayaoh2000@gmail.com>
2022-11-30 12:19:23 -0500

    fix the crashing of the app when pressing the play button

--- app/src/main/java/com/example/cprfeedbackapp/healthMonitoringFragment.java
+++ app/src/main/java/com/example/cprfeedbackapp/healthMonitoringFragment.java
@@ -1,17 +1,14 @@
 package com.example.cprfeedbackapp;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -22,8 +19,6 @@ import java.util.TimerTask;
  * create an instance of this fragment.
  */
 public class healthMonitoringFragment extends Fragment {
-
-    Button emergencyCallButton;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,29 +58,101 @@ public class healthMonitoringFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-            //setting up play and stop sound button
-            View fragmentView = inflater.inflate(R.layout.fragment_health_monitoring,container,false);
-
-
-        //Emergency call button function
-        emergencyCallButton = fragmentView.findViewById(R.id.emergency_button);
-
-        emergencyCallButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent callIntent = new Intent(Intent.ACTION_DIAL); //Call number
-                callIntent.setData(Uri.parse("tel:5149295019")); //call this number
-                startActivity(callIntent);
-            }
-        });
-
         // Inflate the layout for this fragment
-            return fragmentView;
-        }
+        View fragmentView = inflater.inflate(R.layout.fragment_health_monitoring, container, false);
+
+            final MediaPlayer[] mp = {MediaPlayer.create(getActivity(), R.raw.beep)};
+            Button playButton = (Button) fragmentView.findViewById(R.id.play_button);
+
+            //Timer for the interval to play the beep at around 120bpm
+
+        final Timer[] timer = {new Timer("MetronomeTimer", true)};
+            final TimerTask[] tone = {new TimerTask() {
+                @Override
+                public void run() {
+                    //Play sound
+                    mp[0].start();
+                }
+            }};
+
+            //play button
+            playButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(mp[0] == null){
+                        mp[0] = MediaPlayer.create(getActivity(), R.raw.beep);
+                        tone[0] = new TimerTask() {
+                            @Override
+                            public void run() {
+                                mp[0].start();
+                            }
+                        };
+                       /* mp[0].setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mediaPlayer) {
+                                if(mp[0] != null) {
+                                    mp[0].release();
+                                    mp[0] = null;
+                                    Toast.makeText(getActivity(), "Sound Stop Playing", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }); */
+                   }
+
+                    //mp[0].setLooping(true);
+                    //mp[0].start();
+                   timer[0].scheduleAtFixedRate(tone[0], 500, 500); //120 BPM. Executes every 500 ms.
+                    playButton.setEnabled(false);
+                   // handler.postDelayed(runnable, 500);
+
+                }
+            });
+
+            //stop button
+            Button stopButton = (Button) fragmentView.findViewById(R.id.stop_button);
+            stopButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(mp[0] != null) {
+                        mp[0].release();
+                        mp[0] = null;
+                        Toast.makeText(getActivity(), "Sound Stop Playing", Toast.LENGTH_SHORT).show();
+                        playButton.setEnabled(true);
+                        tone[0].cancel();
+                        timer[0].cancel();
+                        timer[0] = new Timer("MetronomeTimer", true);
+
+                        //should stop repeat
+                      //  handler.removeCallbacks(runnable);
+                    }
+                }
+            });
+
+
+        return fragmentView;
+
     }
+
+
+  //  MediaPlayer[] mp = {MediaPlayer.create(getActivity(), R.raw.beep)};
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            //Play sound
+           // mp[0].start();
+
+            //should make sound replay at 500ms = 120bpm
+            //handler.postDelayed(this, 500);
+        }
+    };
+}
+
